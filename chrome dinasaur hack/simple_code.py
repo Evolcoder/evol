e089f6740a6c8c1632c030b415e451008f8c2978
Evolcoder <80657842+Evolcoder@users.noreply.github.com>
2021-04-20 10:26:03 +0530

    Delete chrome dinasaur hack directory

--- chrome dinasaur hack/simple_code.py
+++
@@ -1,8 +0,0 @@
-Runner.instance.gameOver = () => {}
-() => {}
-# put this code into google chrome dinasaur game and you will see a game is sort of hacked
-# Steps
-# Go to the browser 
-# Open the dinasaur game 
-# Right click your mouse button you will see a option inspect
-# And when you click inspect a new panel open and choose the potion console.
